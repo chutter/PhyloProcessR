7368cda4eabf0101bc4b8692ad9532e93cb8f7a4
chutter <carl.hutter@gmail.com>
2020-12-02 12:15:57 -0600

    Update batchTrimAlignments.R

--- R/batchTrimAlignments.R
+++ R/batchTrimAlignments.R
@@ -154,13 +154,13 @@ batchTrimAlignments = function(alignment.dir = NULL,
   save.data[, Pass:=as.logical(Pass)]
 
   #Sets up multiprocessing
-  cl = parallel::makeCluster(threads, outfile = "")
-  doParallel::registerDoParallel(cl)
-  mem.cl = floor(mem/threads)
+  #cl = parallel::makeCluster(threads, outfile = "")
+  #doParallel::registerDoParallel(cl)
+  #mem.cl = floor(mem/threads)
 
   #Loops through each locus and does operations on them
-  out.data = foreach(i=1:length(align.files),.combine = rbind, .packages = c("PHYLOCAP", "foreach", "Biostrings","Rsamtools", "ape", "stringr", "data.table")) %dopar% {
-
+  #out.data = foreach(i=1:length(align.files), .combine = rbind, .packages = c("PHYLOCAP", "foreach", "Biostrings","Rsamtools", "ape", "stringr", "data.table")) %dopar% {
+  for (i in 1:length(align.files)){
     print(paste0(align.files[i], " Started..."))
 
      #Load in alignments
@@ -325,7 +325,7 @@ batchTrimAlignments = function(alignment.dir = NULL,
 
   }#end i loop
 
-  parallel::stopCluster(cl)
+  #parallel::stopCluster(cl)
 
   #Print and save summary table
   write.csv(out.data, file = paste0(output.dir, "_trimming-summary.csv"), row.names = F)
