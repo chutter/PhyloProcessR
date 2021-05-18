638f5d6d99e25f3b3f48a3d7be46556f35ed3324
chutter <carl.hutter@gmail.com>
2021-05-18 02:16:43 -0500

    Update batchTrimAlignments.R

--- R/batchTrimAlignments.R
+++ R/batchTrimAlignments.R
@@ -200,7 +200,7 @@ batchTrimAlignments = function(alignment.dir = NULL,
   mem.cl = floor(memory/threads)
 
   #Loops through each locus and does operations on them
-  out.data = foreach::foreach(i=1:10, .combine = rbind, .packages = c("PhyloCap", "foreach", "Biostrings","Rsamtools", "ape", "stringr", "data.table")) %dopar% {
+  out.data = foreach::foreach(i=1:length(align.files), .combine = rbind, .packages = c("PhyloCap", "foreach", "Biostrings","Rsamtools", "ape", "stringr", "data.table")) %dopar% {
   #for (i in 1:length(align.files)){
     print(paste0(align.files[i], " Starting..."))
 
