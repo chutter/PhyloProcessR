5c2f2c12500be9bb0c9a108363494d6ecd93ce14
chutter <carl.hutter@gmail.com>
2023-04-13 21:36:54 -0500

    update workflows and evs

--- workflows/workflow-5_configuration-file.R
+++ workflows/workflow-5_configuration-file.R
@@ -1,4 +1,4 @@
-#################################################
+################################################
 ## Configuration file for PhyloCap
 #################################################
 
@@ -11,11 +11,11 @@
 #alignment.directory = "/Users/chutter/Dropbox/Research/1_Main-Projects/1_Collaborative-Projects/Guibemantis-liber/alignments/trimmed_all-markers"
 
 #Cluster
-working.directory = "/home/c111h652/scratch/Guibemantis"
+working.directory = "/home/c111h652/scratch/Microhylidae"
 #The file for the contaminant genomes (Genome, Accession columns); use NULL if download.contaminant.genomes = F
-alignment.directory = "/home/c111h652/scratch/Guibemantis/data-analysis/alignments/trimmed_all-markers"
-#The name for the dataset
-dataset.name = "guibemantis"
+alignment.directory = "/home/c111h652/scratch/Microhylidae/data-analysis/alignments/trimmed_all-markers"
+#The name for the alignment dataset
+dataset.name = "trimmed_all-markers"
 
 #Global settings
 #########################
@@ -25,34 +25,13 @@ threads = 16
 memory = 120
 #Whether to overwrite previous runs
 overwrite = FALSE
-#Resume from previous runs (does not overwrite)
-resume = TRUE
 #Print verbose output for each function
 quiet = TRUE
 
-#Filter settings
+#Gene tree estimation settings
 #########################
-
-#minimumn length to keep an alignment (already from trimming step)
-filter.length = NULL
-
-#If you wnat to filter by sampling proportion (percent of taxa to keep an alignment)
-filter.sample = c(0.01)
-
-#not very useful here
-filter.prop.pis = NULL
-
-#Not very useful here
-filter.count.pis = NULL
-
-#Concatenation settings
-#########################
-#select between "concatenated" and "directory"
-#concatenated: a single phylip file of the concatenated loci and also a partition file
-#directory: the filtered set of markers will be saved to a separate folder
-output.type = "concatenated"
 #The minimum number of taxa to keep an alignment
-min.alignments = 5
+min.taxa.tree = 5
 #Select between "file" (by gene from concatenating) or "merge" which is automatic partition finding
 partition.scheme = "merge"
 #Whether to partition by codon position (only if partition.scheme = "file")
@@ -65,13 +44,14 @@ msub.type = "nuclear"
 uf.bootstrap = 1000
 #number of top models to assess for sequence evolution models. Lower number = faster
 rcluster = 10
+#clean up gene trees, deletes intermediate files except ML best tree
+cleanup.genetrees = TRUE
 
 #Program paths
 #########################
 ### *** Modify any of these from NULL to the path that the program is found if R is not detecting system paths
 ### e.g. fastp.path = "/conda/PhyloCap/bin
 conda.env = "/panfs/pfs.local/work/bi/c111h652/conda/PhyloCap/bin/"
-#conda.env = "/usr/local/bin/iqtree2"
 iqtree.path = conda.env
 
 #### End configuration
