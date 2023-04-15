9a1f96576503b058c4d9b04b68feecb339694aeb
chutter <carl.hutter@gmail.com>
2023-04-15 17:32:55 -0500

    update environment file

--- R/assembly.expandMissingAssembly.R
+++ R/assembly.expandMissingAssembly.R
@@ -64,86 +64,103 @@ expandMissingAssembly = function(assembly.directory = NULL,
                                  quiet = TRUE) {
 
   #Debug
-  library(PhyloCap)
+  library(PhyloProcessR)
   setwd("/Volumes/LaCie/Mantellidae")
   assembly.directory = "/Volumes/LaCie/Mantellidae/draft-assemblies"
   output.directory = "expand-missing-assembly"
   reference = "/Volumes/LaCie/Ultimate_FrogCap/Final_Files/FINAL_marker-seqs_Mar14-2023.fa"
   read.directory = "/Volumes/LaCie/Mantellidae/reads"
 
-  iterations = 5
-  spades.path = "/Users/chutter/Bioinformatics/anaconda3/envs/PhyloCap/bin"
-  samtools.path = "/Users/chutter/Bioinformatics/anaconda3/envs/PhyloCap/bin"
-  hisat2.path = "/Users/chutter/Bioinformatics/anaconda3/envs/PhyloCap/bin"
-  blast.path = "/Users/chutter/Bioinformatics/anaconda3/envs/PhyloCap/bin"
-  bwa.path = "/Users/chutter/Bioinformatics/anaconda3/envs/PhyloCap/bin"
-  fastp.path = "/Users/chutter/Bioinformatics/anaconda3/envs/PhyloCap/bin"
-  cdhit.path = "/Users/chutter/Bioinformatics/anaconda3/envs/PhyloCap/bin"
+  spades.path = "/Users/chutter/Bioinformatics/miniconda3/envs/PhyloProcessR/bin"
+  samtools.path = "/Users/chutter/Bioinformatics/miniconda3/envs/PhyloProcessR/bin"
+  hisat2.path = "/Users/chutter/Bioinformatics/miniconda3/envs/PhyloProcessR/bin"
+  blast.path = "/Users/chutter/Bioinformatics/miniconda3/envs/PhyloProcessR/bin"
+  bwa.path = "/Users/chutter/Bioinformatics/miniconda3/envs/PhyloProcessR/bin"
+  fastp.path = "/Users/chutter/Bioinformatics/miniconda3/envs/PhyloProcessR/bin"
+  cdhit.path = "/Users/chutter/Bioinformatics/miniconda3/envs/PhyloProcessR/bin"
 
   mapper = "bbmap"
   quiet = TRUE
-  overwrite = TRUE
+  overwrite = FALSE
   threads = 8
   memory = 1024
   min.match.percent = 60
   min.match.length = 100
   min.match.coverage = 35
 
-  if (is.null(fastp.path) == FALSE){
-    b.string = unlist(strsplit(fastp.path, ""))
+  if (is.null(fastp.path) == FALSE) {
+    b.string <- unlist(strsplit(fastp.path, ""))
     if (b.string[length(b.string)] != "/") {
-      fastp.path = paste0(append(b.string, "/"), collapse = "")
-    }#end if
-  } else { fastp.path = "" }
+      fastp.path <- paste0(append(b.string, "/"), collapse = "")
+    } # end if
+  } else {
+    fastp.path <- ""
+  }
 
-  if (is.null(blast.path) == FALSE){
-    b.string = unlist(strsplit(blast.path, ""))
+  if (is.null(blast.path) == FALSE) {
+    b.string <- unlist(strsplit(blast.path, ""))
     if (b.string[length(b.string)] != "/") {
-      blast.path = paste0(append(b.string, "/"), collapse = "")
-    }#end if
-  } else { blast.path = "" }
+      blast.path <- paste0(append(b.string, "/"), collapse = "")
+    } # end if
+  } else {
+    blast.path <- ""
+  }
 
-  if (is.null(samtools.path) == FALSE){
-    b.string = unlist(strsplit(samtools.path, ""))
+  if (is.null(samtools.path) == FALSE) {
+    b.string <- unlist(strsplit(samtools.path, ""))
     if (b.string[length(b.string)] != "/") {
-      samtools.path = paste0(append(b.string, "/"), collapse = "")
-    }#end if
-  } else { samtools.path = "" }
+      samtools.path <- paste0(append(b.string, "/"), collapse = "")
+    } # end if
+  } else {
+    samtools.path <- ""
+  }
 
-  if (is.null(spades.path) == FALSE){
-    b.string = unlist(strsplit(spades.path, ""))
+  if (is.null(spades.path) == FALSE) {
+    b.string <- unlist(strsplit(spades.path, ""))
     if (b.string[length(b.string)] != "/") {
-      spades.path = paste0(append(b.string, "/"), collapse = "")
-    }#end if
-  } else { spades.path = "" }
+      spades.path <- paste0(append(b.string, "/"), collapse = "")
+    } # end if
+  } else {
+    spades.path <- ""
+  }
 
-  #Same adds to bbmap path
-  if (is.null(hisat2.path) == FALSE){
-    b.string = unlist(strsplit(hisat2.path, ""))
+  # Same adds to bbmap path
+  if (is.null(hisat2.path) == FALSE) {
+    b.string <- unlist(strsplit(hisat2.path, ""))
     if (b.string[length(b.string)] != "/") {
-      hisat2.path = paste0(append(b.string, "/"), collapse = "")
-    }#end if
-  } else { hisat2.path = "" }
+      hisat2.path <- paste0(append(b.string, "/"), collapse = "")
+    } # end if
+  } else {
+    hisat2.path <- ""
+  }
 
-  #Same adds to bbmap path
-  if (is.null(bwa.path) == FALSE){
-    b.string = unlist(strsplit(bwa.path, ""))
+  # Same adds to bbmap path
+  if (is.null(bwa.path) == FALSE) {
+    b.string <- unlist(strsplit(bwa.path, ""))
     if (b.string[length(b.string)] != "/") {
-      bwa.path = paste0(append(b.string, "/"), collapse = "")
-    }#end if
-  } else { bwa.path = "" }
+      bwa.path <- paste0(append(b.string, "/"), collapse = "")
+    } # end if
+  } else {
+    bwa.path <- ""
+  }
 
-  #Same adds to bbmap path
-  if (is.null(cdhit.path) == FALSE){
-    b.string = unlist(strsplit(cdhit.path, ""))
+  # Same adds to bbmap path
+  if (is.null(cdhit.path) == FALSE) {
+    b.string <- unlist(strsplit(cdhit.path, ""))
     if (b.string[length(b.string)] != "/") {
-      cdhit.path = paste0(append(b.string, "/"), collapse = "")
-    }#end if
-  } else { cdhit.path = "" }
+      cdhit.path <- paste0(append(b.string, "/"), collapse = "")
+    } # end if
+  } else {
+    cdhit.path <- ""
+  }
 
-  #Quick checks
-  if (is.null(assembly.directory) == TRUE){ stop("Please provide input reads.") }
-  if (is.null(reference) == TRUE){ stop("Please provide a reference.") }
+  # Quick checks
+  if (is.null(assembly.directory) == TRUE) {
+    stop("Please provide input reads.")
+  }
+  if (is.null(reference) == TRUE) {
+    stop("Please provide a reference.")
+  }
 
   # #Writes reference to file if its not a file path
   # if(file.exists("iterative_temp") == TRUE){ system(paste0("rm -r iterative_temp")) }
@@ -166,41 +183,38 @@ expandMissingAssembly = function(assembly.directory = NULL,
               "qStart", "qEnd", "tStart", "tEnd", "evalue", "bitscore", "qLen", "tLen", "gaps")
 
   save.samples = c()
-  for (i in 1:length(file.names)){
+  for (i in seq_along(file.names)){
 
     #Sets up working directories for each species
     sample = gsub(pattern = ".fa$", replacement = "", x = file.names[i])
     species.dir = paste0(output.directory, "/", sample)
 
-    #Creates species directory if none exists
-    if (file.exists(species.dir) == F){ dir.create(species.dir) }
+    # Creates species directory if none exists
+    if (file.exists(species.dir) == FALSE) {
+      dir.create(species.dir)
+    }
 
-    #Checks if this has been done already
-    if (overwrite == FALSE){
-      if (file.exists(paste0(species.dir, "/", sample, "_matching-contigs.fa")) == T){
-        print(paste0(sample, " already finished, skipping. Set overwrite to T if you want to overwrite."))
-        next
-      }
-    }#end
+    # Make blast database for the probe loci
+    system(paste0(
+      blast.path, "makeblastdb -in ", reference,
+      " -parse_seqids -dbtype nucl -out ", species.dir, "/nucl-blast_db"
+    ), ignore.stdout = quiet)
 
-    #########################################################################
-    #Part A: Blasting
-    #########################################################################
-    #Make blast database for the probe loci
-    system(paste0(blast.path, "makeblastdb -in ", reference,
-                  " -parse_seqids -dbtype nucl -out ", species.dir, "/nucl-blast_db"), ignore.stdout = quiet)
+    # Matches samples to loci
+    system(paste0(
+      blast.path, "blastn -task dc-megablast -db ", species.dir, "/nucl-blast_db -evalue 0.001",
+      " -query ", assembly.directory, "/", file.names[i], " -out ", species.dir, "/target-blast-match.txt",
+      " -outfmt \"6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen slen gaps\" ",
+      " -num_threads ", threads
+    ))
 
-    #Matches samples to loci
-    system(paste0(blast.path, "blastn -task dc-megablast -db ", species.dir, "/nucl-blast_db -evalue 0.001",
-                  " -query ", assembly.directory, "/", file.names[i], " -out ", species.dir, "/target-blast-match.txt",
-                  " -outfmt \"6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen slen gaps\" ",
-                  " -num_threads ", threads))
-
-    #Need to load in transcriptome for each species and take the matching transcripts to the database
+    # Need to load in transcriptome for each species and take the matching transcripts to the database
     system(paste0("rm ", species.dir, "/nucl-blast_db*"))
 
-    #Loads in match data
-    match.data = data.table::fread(paste0(species.dir, "/target-blast-match.txt"), sep = "\t", header = F, stringsAsFactors = FALSE)
+    # Loads in match data
+    match.data = data.table::fread(paste0(species.dir, "/target-blast-match.txt"),
+      sep = "\t", header = FALSE, stringsAsFactors = FALSE
+    )
     data.table::setnames(match.data, headers)
 
     #Matches need to be greater than 12
@@ -209,11 +223,12 @@ expandMissingAssembly = function(assembly.directory = NULL,
     filt.data = filt.data[filt.data$pident >= min.match.percent,]
 
     #Make sure the hit is greater than 50% of the reference length
-    filt.data = filt.data[filt.data$matches >= ( (min.match.coverage/100) * filt.data$tLen),]
+    filt.data = filt.data[filt.data$matches >= ((min.match.coverage / 100) * filt.data$tLen), ]
 
     if (nrow(filt.data) == 0) {
       print(paste0(sample, " had no matches. Skipping"))
-      next }
+      next
+    }
 
     #Sorting: exon name, contig name, bitscore higher first, evalue
     data.table::setorder(filt.data, qName, tName, -pident, -bitscore, evalue)
@@ -224,13 +239,13 @@ expandMissingAssembly = function(assembly.directory = NULL,
     #Finds probes that match to two or more contigs
     final.loci = as.list(as.character(match.contigs))
     writeFasta(sequences = final.loci, names = names(final.loci),
-               paste0(species.dir, "/", sample, "_matching-contigs.fa"), nbchar = 1000000, as.string = T)
+              paste0(species.dir, "/", sample, "_matching-contigs.fa"), nbchar = 1000000, as.string = TRUE)
 
 
     system(paste0("rm ", species.dir, "/target-blast-match.txt"))
 
     write.table(filt.data, file = paste0(species.dir, "/filtered-blast-match.txt"),
-                row.names = F, quote = F, sep = "\t")
+                row.names = FALSE, quote = FALSE, sep = "\t")
 
 
     # miss.reference = reference.seqs[!names(reference.seqs) %in% filt.data$qName]
@@ -242,7 +257,7 @@ expandMissingAssembly = function(assembly.directory = NULL,
     # reference = paste0(species.dir, "/missing_ref.fa")
 
     final.match = c()
-    for (j in 1:nrow(filt.data)){
+    for (j in seq_len(nrow(filt.data))){
       temp.match = match.contigs[names(match.contigs) %in% filt.data$qName[j]]
       names(temp.match) = filt.data$tName[j]
       final.match = append(final.match, temp.match)
@@ -256,7 +271,7 @@ expandMissingAssembly = function(assembly.directory = NULL,
   non.dup = save.samples[!names(save.samples) %in% dup.names]
 
   final.save = c()
-  for (j in 1:length(dup.names)){
+  for (j in seq_along(dup.names)){
     dup.contigs = save.samples[names(save.samples) %in% dup.names[j]]
     save.long = dup.contigs[Biostrings::width(dup.contigs) == max(Biostrings::width(dup.contigs))][1]
     final.save = append(final.save, save.long)
@@ -267,7 +282,7 @@ expandMissingAssembly = function(assembly.directory = NULL,
   #Finds probes that match to two or more contigs
   final.loci = as.list(as.character(final.save))
   writeFasta(sequences = final.loci, names = names(final.loci),
-             paste0(output.directory, "/unique_matches.fa"), nbchar = 1000000, as.string = T)
+            paste0(output.directory, "/unique_matches.fa"), nbchar = 1000000, as.string = TRUE)
 
   #####################################################################################
   #### Read mapping and assembly step
@@ -289,32 +304,43 @@ expandMissingAssembly = function(assembly.directory = NULL,
 
   dir.create("expanded-assemblies")
 
-  for (i in 1:length(file.names)){
+  for (i in seq_along(file.names)){
 
     #Sets up working directories for each species
     sample = gsub(pattern = ".fa$", replacement = "", x = file.names[i])
-    species.dir = paste0(output.directory, "/", sample)
 
-    #Gathers reads
+    # Checks if this has been done already
+    if (overwrite == FALSE) {
+      if (file.exists(paste0("expanded-assemblies/", sample, ".fa")) == TRUE) {
+        print(paste0(sample, " already finished, skipping. Set overwrite to T to overwrite."))
+        next
+      }
+    } # end
+
+    # Gathers reads
     input.reads = read.sets[read.sets == sample]
-    set.reads = list.files(paste0(read.directory, "/", input.reads), full.names = T)
+    set.reads = list.files(paste0(read.directory, "/", input.reads), full.names = TRUE)
 
-    #found data
-    found.data = read.table(paste0(species.dir, "/filtered-blast-match.txt"), sep = "\t", header = T, stringsAsFactors = FALSE)
+    # found data
+    found.data = read.table(paste0(species.dir, "/filtered-blast-match.txt"),
+      sep = "\t", header = TRUE, stringsAsFactors = FALSE
+    )
 
-    #found.data1 = read.table(paste0(species.dir, "/found-missing-blast-match.txt"), sep = "\t", header = T, stringsAsFactors = FALSE)
-    #found.data = rbind(found.data1, found.data2)
+    # found.data1 = read.table(paste0(species.dir, "/found-missing-blast-match.txt"), sep = "\t", header = T, stringsAsFactors = FALSE)
+    # found.data = rbind(found.data1, found.data2)
 
     sample.save = final.save[!names(final.save) %in% found.data$tName]
 
-    #Finds probes that match to two or more contigs
+    # Finds probes that match to two or more contigs
     final.loci = as.list(as.character(sample.save))
-    writeFasta(sequences = final.loci, names = names(final.loci),
-               paste0(species.dir, "/missing_ref.fa"), nbchar = 1000000, as.string = T)
+    writeFasta(
+      sequences = final.loci, names = names(final.loci),
+      paste0(species.dir, "/missing_ref.fa"), nbchar = 1000000, as.string = T
+    )
 
     missing.ref = paste0(species.dir, "/missing_ref.fa")
 
-    #missing.ref = "/Volumes/LaCie/Mantellidae/find-missing-asssemble/Blommersia_grandisonae_CRH-792/Blommersia_grandisonae_CRH-792_matching-contigs.fa"
+    # missing.ref = "/Volumes/LaCie/Mantellidae/find-missing-asssemble/Blommersia_grandisonae_CRH-792/Blommersia_grandisonae_CRH-792_matching-contigs.fa"
 
     #Creates reference
     dir.create(paste0(species.dir, "/index"))
@@ -328,18 +354,32 @@ expandMissingAssembly = function(assembly.directory = NULL,
 
     # sample.reads <- unique(gsub("_1.f.*|_2.f.*|_3.f.*|-1.f.*|-2.f.*|-3.f.*|_R1_.*|_R2_.*|_R3_.*|_READ1_.*|_READ2_.*|_READ3_.*|_R1.f.*|_R2.f.*|_R3.f.*|-R1.f.*|-R2.f.*|-R3.f.*|_READ1.f.*|_READ2.f.*|_READ3.f.*|-READ1.f.*|-READ2.f.*|-READ3.f.*|_singleton.*|-singleton.*|READ-singleton.*|READ_singleton.*|_READ-singleton.*|-READ_singleton.*|-READ-singleton.*|_READ_singleton.*", "", sample.reads))
 
-
     # Here is running hisat2
     system(paste0(hisat2.path, "hisat2 -q -x ", species.dir, "/index/reference",
                   " -1 ", set.reads[1], " -2 ", set.reads[2],
-                  " -S ", species.dir, "/mapped_reads.sam --mp 1,0 --sp 1,0 --score-min L,0.0,-0.4",
+                  " -S ", species.dir, "/mapped_reads.sam --mp 1,0 --sp 1,0 --score-min L,0.0,-0.3",
                   " --threads ", threads))
 
-    system(paste0(samtools.path, "samtools view -@ ", threads, " -b -F 4 ", species.dir, "/mapped_reads.sam > ", species.dir, "/mapped_all.sam"))
-    system(paste0(samtools.path, "samtools view -@ ", threads, " -b -f 4 -F 264 ", species.dir, "/mapped_reads.sam > ", species.dir, "/mapped1.sam"))
-    system(paste0(samtools.path, "samtools view -@ ", threads, " -b -f 8 -F 260 ", species.dir, "/mapped_reads.sam > ", species.dir, "/mapped2.sam"))
-    system(paste0(samtools.path, "samtools merge ", species.dir, "/mapped_combined.sam ", species.dir, "/mapped_all.sam ", species.dir, "/mapped1.sam ", species.dir, "/mapped2.sam"))
-    system(paste0(samtools.path, "samtools sort -n ", species.dir, "/mapped_combined.sam > ", species.dir, "/mapped_sort.sam"))
+    system(paste0(
+      samtools.path, "samtools view -@ ", threads, " -b -F 4 ",
+      species.dir, "/mapped_reads.sam > ", species.dir, "/mapped_all.sam"
+    ))
+    system(paste0(
+      samtools.path, "samtools view -@ ", threads, " -b -f 4 -F 264 ",
+      species.dir, "/mapped_reads.sam > ", species.dir, "/mapped1.sam"
+    ))
+    system(paste0(
+      samtools.path, "samtools view -@ ", threads, " -b -f 8 -F 260 ",
+      species.dir, "/mapped_reads.sam > ", species.dir, "/mapped2.sam"
+    ))
+    system(paste0(
+      samtools.path, "samtools merge ", species.dir, "/mapped_combined.sam ",
+      species.dir, "/mapped_all.sam ", species.dir, "/mapped1.sam ", species.dir, "/mapped2.sam"
+    ))
+    system(paste0(
+      samtools.path, "samtools sort -n ",
+      species.dir, "/mapped_combined.sam > ", species.dir, "/mapped_sort.sam"
+    ))
 
     # system(paste0(samtools.path, "samtools flagstat ", species.dir, "/mapped_sort.sam"))
     #
@@ -360,7 +400,7 @@ expandMissingAssembly = function(assembly.directory = NULL,
                         threads = threads,
                         mem = memory,
                         overwrite = overwrite,
-                        quiet = F)
+                        quiet = FALSE)
 
     #Removes if nothing merged
     if (file.size(paste0(species.dir, "/temp-merged-reads/sample/sample_READ3.fastq.gz")) == 0){
@@ -379,13 +419,13 @@ expandMissingAssembly = function(assembly.directory = NULL,
 
     #Runs spades
     spades.contigs = runSpades(read.paths = temp.read.path,
-                               full.path.spades = spades.path,
-                               mismatch.corrector = T,
-                               isolate = F,
-                               quiet = quiet,
-                               read.contigs = T,
-                               threads = threads,
-                               memory = memory)
+                              full.path.spades = spades.path,
+                              mismatch.corrector = TRUE,
+                              isolate = FALSE,
+                              quiet = quiet,
+                              read.contigs = TRUE,
+                              threads = threads,
+                              memory = memory)
 
     spades.contigs = spades.contigs[Biostrings::width(spades.contigs) >= 100]
 
